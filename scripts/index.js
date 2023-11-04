d8ac3268ab70d8f7b53d5c21b592d87429e68f4f
Katerina Safiullova <e.safiullova2000@gmail.com>
2023-11-04 20:20:16 +0500

    добавила кнопку прокрутки страницы наверх, подредактировала домашнюю страницу

--- scripts/index.js
+++ scripts/index.js
@@ -1,63 +1,40 @@
-// для "перехода" с домашней страницы на JS
-
-// const homePage = document.querySelector('.home');
-// const btnJS = document.querySelector('.home__row1-item4');
-const cardList = document.querySelector('.cards');
-const demo = document.querySelector('.demo');
-const loader = document.querySelector('.loader');
-
-function addClassNone (item)  {
-  item.classList.add("none");
-  };
-  
-  function delClassNone (item)  {
-    item.classList.remove("none");
-    };
-
-function start () {
-  setTimeout(autoComplete, 3000);
-};
-
-start ();
-
 // первым блоком script для модального окна и формы отправки
 
 // для строки поиска
 
 // для отрисовки карточек и их анимации
 
+const cardList = document.querySelector('.cards');
+const demo = document.querySelector('.demo');
+const loader = document.querySelector('.loader');
+
+window.setTimeout(() => {
+  autoComplete();
+
+}, 3000);
+
 // const modal = document.querySelector('#modal');
 // const close = document.querySelector('.modal__card-close');
-
-
-
 
 // close.onclick = function () {
 //   modal.style.display = 'none';
 // };
 
-// window.onclick = function (event) {
-//   if (event.target == modal) {
-//     modal.style.display = 'none';
-//   }
-// };
-
-  
-
 function autoComplete () {
   fetch ("http://localhost:8080/cards")
+
   .then((response) => {
     return response.json();
   })
+
   .then((data) => {
-    // const namesList = data;
     // const namesList = data.map(item => item.name);
 
     let card = "";
+
     for (let i = 0; i <= data.length; i++) {
-const item = new Object(data[i]);
-// const cardName = item['name'];
-// const cardDescription = item['description'];
+
+      const item = new Object(data[i]);
       card += ` <label>
         <input type="checkbox"  />
         <div class="card">
@@ -70,29 +47,72 @@ const item = new Object(data[i]);
           </div>
         </div>
       </label>`
-    }
+    };
+
     cardList.innerHTML = card;
-    
-    // cardList.onclick = function (event) {
-    //   let div = event.target.closest('div'); 
-    //   const children = div.lastElementChild;
-    //   if (!div) return; 
-    //   if (!cardList.contains(div)) return; 
-            // modal.style.display = 'block';
-      // children.classList.toggle('none');
-      // div.style.height = "200px";
-    // }
-    
   })
+
   .catch ((e) => {
     console.error(e);
-    // demo.style.display = 'flex';
-    delClassNone (demo)
+    demo.classList.remove("none");
   })
+
   .finally(() => {
     loader.classList.add("none");
   });
 };
 
-// autoComplete();
-// myFunction ();
+
+const btnUp = {
+  el: document.querySelector('.btn-up'),
+  scrolling: false,
+  show() {
+    if (this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
+      this.el.classList.remove('btn-up_hide');
+      this.el.classList.add('btn-up_hiding');
+      window.setTimeout(() => {
+        this.el.classList.remove('btn-up_hiding');
+      }, 300);
+    }
+  },
+  hide() {
+    if (!this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
+      this.el.classList.add('btn-up_hiding');
+      window.setTimeout(() => {
+        this.el.classList.add('btn-up_hide');
+        this.el.classList.remove('btn-up_hiding');
+      }, 300);
+    }
+  },
+  addEventListener() {
+    // при прокрутке окна (window)
+    window.addEventListener('scroll', () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      if (this.scrolling && scrollY > 0) {
+        return;
+      }
+      this.scrolling = false;
+      // если пользователь прокрутил страницу более чем на 200px
+      if (scrollY > 400) {
+        // сделаем кнопку .btn-up видимой
+        this.show();
+      } else {
+        // иначе скроем кнопку .btn-up
+        this.hide();
+      }
+    });
+    // при нажатии на кнопку .btn-up
+    document.querySelector('.btn-up').onclick = () => {
+      this.scrolling = true;
+      this.hide();
+      // переместиться в верхнюю часть страницы
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+  }
+}
+
+btnUp.addEventListener();
